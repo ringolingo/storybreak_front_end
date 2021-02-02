c6f34213ec0c7b2ecaa65b7f318ca9119ce8db8c
Ringo Alcock <ringo.alcock@gmail.com>
2021-02-01 16:54:48 -0800

    set up bootstrap and react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import WritingDesk from './components/WritingDesk';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          {/* can I add a ternary or something (maybe function?) so this only displays
+          for the writing desk or corkboard view and not if logged out or on story list */}
+          <nav>
+            <ul>
+              <li>
+                <Link to='/desk'>Writing Desk</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path='/desk'>
+              <WritingDesk />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+      <p>footer placeholder</p>
     </div>
   );
 }
