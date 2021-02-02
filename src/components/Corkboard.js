227cbaee5ce4e511126d8523ffb16564945047a7
Ringo Alcock <ringo.alcock@gmail.com>
2021-02-02 13:22:07 -0800

    add modal and form for new/edit cards

--- src/components/Corkboard.js
+++ src/components/Corkboard.js
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 import IndexCard from './IndexCard';
 import './Corkboard.css';
+
 
 const placeholderCards = [
     {
@@ -23,18 +28,70 @@ const placeholderCards = [
 
 const Corkboard = () => {
     const [cards, setCards] = useState(placeholderCards);
+    const [showModal, setShowModal] = useState(false);
+    const [currentCard, setCurrentCard] = useState({
+        id: null,
+        logline: '',
+    });
+
+    const popOutCard = (card) => {
+        const selectedCard = {
+            id: card.id,
+            logline: card.logline,
+        }
+        setCurrentCard(selectedCard);
+        setShowModal(true);
+    }
+    
+    const closeModal = () => {
+        setShowModal(false);
+        setCurrentCard({
+            id: null,
+            logline: null,
+        })
+    }
 
     const cardComponents = cards.map((card) => {
         return (
-            <IndexCard logline={card.logline} key={card.id}/>
+            <IndexCard logline={card.logline} key={card.id} id={card.id} showCard={popOutCard} />
         )
     });
 
     // TODO refactor to connect with backend
     const addCard = () => {
         const expandedCards = [...cards];
-        expandedCards.push(<IndexCard logline='' key={cards.length + 1} />);
+        const newCard = {
+            id: cards.length + 1,
+            logline: '',
+        }
+        expandedCards.push(newCard);
         setCards(expandedCards);
+
+        popOutCard(newCard);
+    };
+
+    const changeCardSummary = (event) => {
+        const updatedCard = {
+            id: currentCard.id,
+            logline: event.target.value,
+        };
+
+        setCurrentCard(updatedCard);
+    };
+
+    const saveCardChanges = () => {
+        const updatedCards = [];
+        
+        cards.forEach((card) => {
+            if (card.id === currentCard.id) {
+                updatedCards.push(currentCard);
+            } else {
+                updatedCards.push(card);
+            }
+        });
+
+        setCards(updatedCards);
+        closeModal();
     };
 
     return (
@@ -46,6 +103,36 @@ const Corkboard = () => {
             <div className="corkboard__button-bar d-flex justify-content-center">
                 <button onClick={addCard} className="btn btn-medium btn-primary">Add New Card</button>
             </div>
+
+            <Modal 
+                show={showModal}
+                onHide={closeModal}
+                animation={false}
+                backdrop='static'
+                centered={true}
+            >    
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Scene Summary</Form.Label>
+                            <Form.Control
+                                as='textarea'
+                                value={currentCard.logline}
+                                onChange={changeCardSummary}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+            
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={closeModal}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={saveCardChanges}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 };
